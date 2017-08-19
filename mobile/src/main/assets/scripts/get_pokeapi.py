5ba0a9aa605b646d467edf259653040b9997b6d8
Amaredeus <patinobrian@gmail.com>
2017-08-19 11:47:28 -0400

    progress

--- mobile/src/main/assets/scripts/get_pokeapi.py
+++ mobile/src/main/assets/scripts/get_pokeapi.py
@@ -1,15 +1,28 @@
 import requests
 import json
 
-dataString = 'type'
+dataString = 'item-flind-effect'
 startId = 1
-endId = 3
-baseUrl = 'https://pokeapi.co/api/v2/'
+endId = None
+baseUrl = 'https:localhost:8000/api/v2'
 
-for dataId in range(startId, endId):
+while True:
     # Call the poke moves request
-    dateJson = requests.get(baseUrl + dataString + '/' + str(dataId)).json()
-    fileName = 'poke_'+ dataString + '_'+ str(dataId) + '.json'
-    with open('../in/' + fileName, 'x') as dataFile:
-        json.dump(dateJson, dataFile)
+    response = requests.get(baseUrl + dataString + '/' + str(startId))
+    
+    if response.status_code == requests.codes.not_found:
+        print('Finished.')
+        break
+
+    dataJson = response.json()
+
+    fileName = 'poke_'+ dataString + '_'+ str(startId) + '.json'
+    with open('../in/' + fileName, 'w') as dataFile:
+        json.dump(dataJson, dataFile)
         print(fileName)
+
+    startId += 1
+
+    if endId != None and startId >= endId:
+        print('Finished.')
+        break
---
+++ mobile/src/main/assets/scripts/shared.py
@@ -0,0 +1,28 @@
+baseUrl = 'http://localhost:8000/api/v2/'
+
+fileLangs = ['en', 'es', 'de', 'fr', 'it', 'ja', 'ko', 'zh', 'cs']
+gens = {'genId-i': 1, 'genId-ii': 2, 'genId-iii': 3, 'genId-iv': 4, 'genId-v': 5, 'genId-vi': 6}
+pokeTypes = {'normal': 1, 'fire': 2, 'water': 3, 'electric': 4, 'grass': 5, 'ice': 6, 'fighting': 7, 'poison': 8, 'ground': 9, 'flying': 10, 'psychic': 11, 'bug': 12, 'rock': 13, 'ghost': 14, 'dragon': 15, 'dark': 16, 'steel': 17, 'fairy': 18, '???': 19}
+dmgTypes = {'physical': 1, 'special': 2, 'status': 3}
+contestTypes = {'cool': 1, 'beautiful': 2, 'beauty': 2, 'cute': 3, 'clever': 4, 'smart': 4, 'tough': 5}
+
+# Gen pokeData Langs
+pokeDataLangs = {'stats': []}
+for lang in fileLangs:
+    pokeDataLangs[lang] = []
+
+
+def getLocalText(lang, textType, listOfTexts):
+    for text in listOfTexts:
+        if (text['language']['name'][:2].lower() == lang):
+            return text[textType]
+    return None
+
+def getObjName(customObj, fieldName):
+    if (customObj == None):
+        return None
+    customName = customObj[fieldName]
+    if (customName == None):
+        return None
+    customName.lower()
+    return customObj[customName]
